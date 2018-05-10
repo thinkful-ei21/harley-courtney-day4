bd8b3764baa14e0c71e6229b9ff3f4ca3b0fdf7f
harleyjj <harleyyesm@gmail.com>
2018-05-10 14:38:18 -0700

    final updates of pair sesssion but unfinished

--- fizzBuzz/index.js
+++ fizzBuzz/index.js
@@ -2,18 +2,37 @@
 
 function fizzBuzz(num) {
 
-	if(num % 15 === 0) return 'fizzbuzz';
-	if(num % 5 === 0) return 'buzz';
-	if(num % 3 === 0) return 'fizz';
-	return num;
+  if(num % 15 === 0) return; 
+  `<div class="fizz-buzz-item fizzbuzz">
+  <span>fizzbuzz</span>
+  </div>`;
+  if(num % 5 === 0) return; 
+  `<div class="fizz-buzz-item buzz">
+  <span>buzz</span>
+  </div>`;
+  if(num % 3 === 0) return 
+  `<div class="fizz-buzz-item fizz">
+  <span>fizz</span>
+  </div>`;
+  return `<div class="fizz-buzz-item">
+  <span>1</span>
+  </div>`;
+}
+
+function generateValues(num) {
+  let result = [];
+  for(let i = 1; i <= num; i++){
+    result.append(fizzBuzz(num));
+  }
+  return result;
 }
 
 
 function getUserValue() {
-	$('#number-chooser').on('submit', event => {
+  $('#number-chooser').on('submit', event => {
     	event.preventDefault();
-    	let num = parseInt($('#number-choice').val(), 10);
-    });
+    	let valueArray = generateValues(parseInt($('#number-choice').val(), 10));
+  });
 }
 
 
